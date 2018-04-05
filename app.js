cd249a95d3736910bec2cab9d858c9702b3ada41
Olaf Vaher <olafvaher@Olafs-MacBook-Pro.local>
2018-04-05 13:58:55 +0200

    basic portfolio site setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+// set up body parser and a css files
+app.use(express.static(__dirname + '/'));
+
+// run either in production or dev
+app.set('port', (process.env.PORT || 5000));
+
+// basic routes
+app.get("/", function(req, res) {
+    res.sendFile('index.html', {"root": __dirname});
+});
+
+
+app.listen(app.get('port'), function() {
+    console.log("Server started.");
+});
