03fe50139d809f873ecc575f5560929562ccd9f2
Olaf Vaher <olafvaher@Olafs-MacBook-Pro.local>
2018-04-09 19:01:32 +0200

    test ping

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ app.set('port', (process.env.PORT || 5000));
 
 // ping app in every 10 minutes (600000)
 setInterval(function() {
-    http.get("https://olafvaher.herokuapp.com");
-}, 600000);
+    http.get("http://olafvaher.herokuapp.com");
+    console.log("pinged");
+}, 6000);
 
 // basic routes
 app.get("/", function(req, res) {
