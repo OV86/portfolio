3e442e676ac131dc29fa79e7f895fe5c79caaf00
Olaf Vaher <olafvaher@Olafs-MacBook-Pro.local>
2018-05-04 11:37:04 +0200

    added get to missing portfolio items

--- app.js
+++ app.js
@@ -9,27 +9,29 @@ app.use(express.static(__dirname + '/'));
 app.set('port', (process.env.PORT || 5000));
 
 // ping app in every 10 minutes (600000)
-setInterval(function() {
+setInterval(function () {
     http.get("http://olafvaher.herokuapp.com");
+    http.get("https://gentle-anchorage-53988.herokuapp.com");
+    http.get("https://quiet-waters-93112.herokuapp.com");
 }, 600000);
 
 // basic routes
-app.get("/", function(req, res) {
-    res.sendFile('index.html', {"root": __dirname});
+app.get("/", function (req, res) {
+    res.sendFile('index.html', { "root": __dirname });
 });
 
-app.get("/budgety-app", function(req, res) {
-    res.sendFile('/budgety-app/index.html', {"root": __dirname});
+app.get("/budgety-app", function (req, res) {
+    res.sendFile('/budgety-app/index.html', { "root": __dirname });
 });
 
-app.get("/pig-game", function(req, res) {
-    res.sendFile('/pig-game/index.html', {"root": __dirname});
+app.get("/pig-game", function (req, res) {
+    res.sendFile('/pig-game/index.html', { "root": __dirname });
 });
 
-app.get("/random-quote", function(req, res) {
-    res.sendFile('/random-quote/index.html', {"root": __dirname});
+app.get("/random-quote", function (req, res) {
+    res.sendFile('/random-quote/index.html', { "root": __dirname });
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log("Server started.");
 });
